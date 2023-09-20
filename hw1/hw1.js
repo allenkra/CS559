d5150a25bd11e2b13fac7ba00fe6796ac8d88c24
Kkerhialoxy <allen.wwhl@gmail.com>
2023-09-20 18:48:43 -0500

    basic drawing completed

--- hw1/hw1.js
+++ hw1/hw1.js
@@ -2,7 +2,68 @@ function setup() {
     var Canvas = document.getElementById("myCanvas");
     var CannonBallSizeSlider = document.getElementById("sizeSlider");
     var PositionSlider = document.getElementById("positionSlider");
-    function drawWarship() {
+    
+    function draw() {
+        var context=Canvas.getContext('2d');
+
+        function drawWarship(){
+            // draw body
+            context.lineWidth = 5;
+            context.fillStyle = "green";
+            context.beginPath();
+            context.moveTo(100,200);
+            context.lineTo(450,200);
+            context.lineTo(350,280);
+            context.lineTo(130,280);
+            context.closePath();
+            context.fill()
+
+            // draw top
+            context.beginPath();
+            context.strokeStyle="blue";
+            context.moveTo(200,200);
+            context.lineTo(200,80);
+            context.lineTo(280,80);
+            context.lineTo(280,200);
+            context.stroke();
+
+            // draw cannon
+            context.beginPath()
+            context.strokeStyle="black";
+            context.lineWidth = 8;
+            context.moveTo(260,110);
+            context.lineTo(460,80);
+            context.moveTo(260,160);
+            context.lineTo(460,190);
+            context.stroke();
+        }
+
+        function drawFlag(){
+            context.beginPath();
+            context.strokeStyle="black";
+            context.lineWidth = 6;
+            context.moveTo(145,200);
+            context.lineTo(145,60);
+            context.stroke();
+
+            context.beginPath();
+            context.fillStyle="yellow";
+            context.strokeStyle="black";
+            context.moveTo(145,60);
+            context.lineTo(50,100);
+            context.lineTo(145,140);
+            context.closePath();
+            context.fill();
+            context.stroke()
         
+        }
+
+        drawWarship();
+        context.save();
+
+        context.restore();
+        drawFlag();
     }
+    draw();
 }
+window.onload = setup;
