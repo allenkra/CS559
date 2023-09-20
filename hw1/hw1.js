aee848c64ed03a821408d81c05fe45d8dacfd9ba
Kkerhialoxy <allen.wwhl@gmail.com>
2023-09-20 17:56:30 -0500

    warshipdemo

---
+++ hw1/hw1.js
@@ -0,0 +1,8 @@
+function setup() {
+    var Canvas = document.getElementById("myCanvas");
+    var CannonBallSizeSlider = document.getElementById("sizeSlider");
+    var PositionSlider = document.getElementById("positionSlider");
+    function drawWarship() {
+        
+    }
+}
