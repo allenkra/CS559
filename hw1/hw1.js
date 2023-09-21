e3e2dbdbdfe945d41b33379810a27a417516138d
Kkerhialoxy <allen.wwhl@gmail.com>
2023-09-20 19:36:00 -0500

    clean without cannon ball

--- hw1/hw1.js
+++ hw1/hw1.js
@@ -2,13 +2,10 @@ function setup() {
     var Canvas = document.getElementById("myCanvas");
     var CannonBallSizeSlider = document.getElementById("sizeSlider");
     var PositionSlider = document.getElementById("positionSlider");
-    var firing = false;
     
     function draw() {
         var context=Canvas.getContext('2d');
         dy = PositionSlider.value;
-        var cannonballX = 220; // initial location of cannonball
-        var cannonballSize = CannonBallSizeSlider.value;
         function drawWarship(){
 
             // clear Canvas
@@ -65,31 +62,7 @@ function setup() {
         
         }
 
-        function drawCannonBall(){
-            // Draw the cannonball
-            if (firing) {
-                ctx.beginPath();
-                ctx.arc(cannonballX, 425, cannonballSize, 0, Math.PI * 2);
-                ctx.fillStyle = "black";
-                ctx.fill();
-                ctx.closePath();
-                cannonballX += 10;
-
-                if (cannonballX > 800) {
-                    firing = false;
-                    cannonballX = 220; // Relocate cannonball when out of canvas
-                }
-            } else {
-                ctx.beginPath();
-                ctx.arc(cannonballX, 425, cannonballSize, 0, Math.PI * 2);
-                ctx.fillStyle = "black";
-                ctx.fill();
-                ctx.closePath();
-            }
-        }
-
         drawWarship();
-        requestAnimationFrame(drawCannonBall);
         context.save();
         context.translate(0,dy);
         drawFlag();
@@ -97,11 +70,8 @@ function setup() {
         context.restore();
     }
     PositionSlider.addEventListener("input",draw);
-    CannonBallSizeSlider.addEventListener("input",draw);
+    CannonBallSizeSlider.addEventListener("input",draw)
     draw();
 }
 // Button to fire the cannon
-function fireCannon() {
-    firing = true;
-}
 window.onload = setup;
